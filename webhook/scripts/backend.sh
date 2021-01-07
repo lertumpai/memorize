b9a4f94b3db88a9972658f836719219646977e58
ninew <new-sorawit@outlook.com>
2021-01-08 03:23:11 +0700

    remove unused

--- webhook/scripts/backend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ./application/backend
-
-docker-compose down --rmi all
-docker-compose up -d
--- webhook/scripts/frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ./application/frontend
-
-docker-compose down --rmi all
-docker-compose up -d
